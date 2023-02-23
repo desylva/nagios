d2d7d9e114373cbeed90a380c5b566b775adea74
Olivier <olivier@Oliviers-MacBook-Pro.local>
2023-02-23 00:29:34 +0000

    Handling some of the errors received from the API

--- check_qualys/src/main.rs
+++ check_qualys/src/main.rs
@@ -1,79 +1,96 @@
 use clap::{Arg, ArgMatches, Command};
 use reqwest::blocking::get;
-use scraper::{Html, Selector};
-use std::fs;
-use std::io;
+use serde::{Deserialize, Serialize};
+use std::process::exit;
 use std::thread;
 use std::time::Duration;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let cli_options = get_cli_parameters();
-    let mut is_ready = false;
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Endpoint {
+    status_message: Option<String>,
+    grade: Option<String>,
+}
 
-    while is_ready == false {
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Response {
+    host: String,
+    status: String,
+    status_message: Option<String>,
+    endpoints: Option<Vec<Endpoint>>,
+}
+
+#[derive(Debug)]
+struct Status {
+    ready: bool,
+    status: String,
+    grade: String,
+    message: String,
+    code: i32,
+}
+
+fn main() {
+    let cli_options = get_cli_parameters();
+    let mut status = Status {
+        ready: false,
+        status: "".to_string(),
+        grade: "".to_string(),
+        message: "".to_string(),
+        code: 0,
+    };
+
+    let mut count = 0;
+    while status.ready == false {
+        count += 1;
+        // println!("Count: {}", count);
         let mut body = "".to_string();
-        if let Some(file) = cli_options.get_one::<String>("file") {
-            body = get_file_body(file);
-        } else if let Some(url) = cli_options.get_one::<String>("url") {
-            body = get_url_body(url);
+
+        if let Some(url) = cli_options.get_one::<String>("url") {
+            body = get_url_body(url.to_string());
+        } else {
         }
 
-        is_ready = process_response_body(body);
+        status = process_response_body(body, status);
 
-        if is_ready == false {
-            let pause_duration = Duration::from_secs(5);
+        if status.ready == false {
+            let pause_duration = Duration::from_secs(10);
             thread::sleep(pause_duration);
+        }
+        if count > 5 {
+            break;
         }
     }
 
-    return Ok(());
+    print_result(&status);
+    exit(status.code);
 }
 
-fn get_url_body(url: &str) -> String {
+fn get_url_body(url: String) -> String {
     let request_url = format!(
         "{}{}{}",
-        "https://www.ssllabs.com/ssltest/analyze.html?d=".to_string(),
-        url,
-        "&hideResults=on&latest".to_string()
+        "https://api.ssllabs.com/api/v3/analyze?host=", url, "&publish=off&fromCache=on"
     );
-    // println!("{}", request_url);
     let response = get(request_url).unwrap();
 
     // Capture the response body
     let body = response.text().unwrap();
-    return body;
-}
-
-fn get_file_body(file: &str) -> String {
-    match open_file(file) {
-        Ok(file) => {
-            return file;
-        }
-        Err(error) => {
-            // Handle the error
-            eprintln!("Failed to open file: {}", error);
-        }
-    }
-    return "".to_string();
-}
-
-fn open_file(file_path: &str) -> Result<String, io::Error> {
-    let file_result = fs::read_to_string(file_path)?;
-    Ok(file_result)
+    // println!("{}", body);
+    body
 }
 
 fn get_cli_parameters() -> ArgMatches {
     let matches = Command::new("qualysapp")
         .arg(
-            Arg::new("file")
-                .help("Input file path")
-                .short('f')
-                .required(false),
-        )
-        .arg(
             Arg::new("url")
                 .help("URL address")
                 .short('u')
+                .required(false),
+        )
+        .arg(
+            Arg::new("time")
+                .help("Time to pause between request attempts to the API")
+                .short('t')
                 .required(false),
         )
         .get_matches();
@@ -81,63 +98,104 @@ fn get_cli_parameters() -> ArgMatches {
     return matches;
 }
 
-fn process_response_body(body: String) -> bool {
-    let document = Html::parse_document(&body);
-    let mut ready_status = false;
-
-    // CDN case
-    let cdn_table_selector = Selector::parse("table#multiTable tbody").unwrap();
-    let cdn_row_selector = Selector::parse("tr").unwrap();
-    // IP case
-    let ip_selector = Selector::parse("div.reportTitle").unwrap();
-    let ip_selector_rating = Selector::parse("div.rating_g").unwrap();
-
-    // Find the table
-    let html_result = match document.select(&cdn_table_selector).next() {
-        Some(html) => (true, html),
-        None => {
-            if let Some(html_div) = document.select(&ip_selector).next() {
-                // println!("{}", html_div.inner_html().as_str());
-                (false, html_div)
+fn process_response_body(body: String, mut status: Status) -> Status {
+    let response: Response = serde_json::from_str(&body).unwrap();
+    // i.e. Unable to resolve domain name
+    status = set_response_status(status, &response);
+    // Continue otherwise
+    match response.endpoints {
+        Some(endpoints) => {
+            if let Some(endpoint) = endpoints.first() {
+                let grade = endpoint
+                    .grade
+                    .as_ref()
+                    .map(String::as_str)
+                    .unwrap_or_default();
+                let status_message = endpoint
+                    .status_message
+                    .as_ref()
+                    .map(String::as_str)
+                    .unwrap_or_default();
+                status = set_status(status, status_message);
+                status.grade = grade.to_string();
+                status = set_exit_code(status);
             } else {
-                panic!("Failed to find rating for a CDN or IP case");
+                status.code = 3;
+                status.status = "ERROR".to_string();
+                status.message = "Endpoint not ready".to_string();
             }
         }
-    };
-
-    if html_result.0 == true {
-        // Find the first non-title row
-        let first_row = html_result
-            .1
-            .select(&cdn_row_selector)
-            .skip(2) // skip the title row
-            .next()
-            .unwrap();
-
-        // Extract the ready status
-        let cell_result_selector = Selector::parse("td").unwrap();
-        let result_cell = first_row.select(&cell_result_selector);
-        for cell in result_cell.clone() {
-            if cell.text().collect::<String>().contains("Ready") {
-                ready_status = true;
-            }
+        None => {
+            status.code = 3;
+            status.status = "ERROR".to_string();
+            status.message = "No endpoint".to_string();
         }
-
-        // Extract the result value
-        let result = result_cell.last();
-        if let Some(cell) = result {
-            println!("{}", cell.text().collect::<String>().trim());
-        }
-        return ready_status;
-    } else {
-        let ip_rating = match document.select(&ip_selector_rating).next() {
-            Some(html) => html,
-            None => {
-                return false;
-            }
-        };
-        println!("{}", ip_rating.inner_html().trim());
-        ready_status = true;
-        return ready_status;
     }
+    status
+}
+
+fn set_response_status(mut status: Status, response: &Response) -> Status {
+    match response.status.as_str() {
+        "DNS" => status.status = "DNS".to_string(),
+        "ERROR" => status.status = "ERROR".to_string(),
+        "IN_PROGRESS" => status.status = "IN_PROGRESS".to_string(),
+        "READY" => status.status = "OK".to_string(),
+        _ => status.status = "UNKOWN".to_string(),
+    }
+
+    if status.status == "ERROR" {
+        status.message = response
+            .status_message
+            .as_ref()
+            .map(String::as_str)
+            .unwrap_or_default()
+            .to_string();
+        status.code = 3;
+        status.ready = true;
+        return status;
+    } else if status.status == "IN_PROGRESS" || status.status == "READY" || status.status == "DNS" {
+        status.code = 0;
+        status.ready = false;
+        return status;
+    } else {
+        status.message = response
+            .status_message
+            .as_ref()
+            .map(String::as_str)
+            .unwrap_or_default()
+            .to_string();
+        status.code = 3;
+        status.ready = true;
+        return status;
+    }
+}
+
+fn set_status(mut status: Status, status_message: &str) -> Status {
+    status.status = status_message.to_string();
+    if status.status == "Ready" {
+        status.ready = true;
+    }
+    status
+}
+
+fn set_exit_code(mut status: Status) -> Status {
+    match status.grade.as_str() {
+        "A" | "A+" => status.code = 0,
+        "A-" => status.code = 1,
+        "B" | "C" | "D" | "E" | "F" | "M" | "T" => status.code = 2,
+        _ => status.code = 2,
+    }
+    status
+}
+
+fn print_result(status: &Status) {
+    if status.grade.is_empty() && !status.message.is_empty() {
+        println!("{}: {}", status.status, status.message);
+    } else if !status.grade.is_empty() && status.message.is_empty() {
+        println!("{}", status.grade);
+    } else {
+        println!("{} {}: {}", status.grade, status.status, status.message);
+    }
+    // println!("Print all anyway: {} {}", status.grade, status.message);
+    // println!("{:?}", status);
 }
